ae5e9294d5170356a955cbc83f8c688c2eb585b1
babuacc <travis.mar88@gmail.com>
2021-11-10 01:49:01 +0530

    sampleone

---
+++ src/org/company/Company.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class Company {
+	
+	public void companyName() {
+	System.out.println("COMPANY NAME IS CTS");
+
+	}
+
+	public void companyLocation() {
+	System.out.println("COMPANY LOCATION IS OMR");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Company c = new Company();
+		
+		c.companyName();
+		c.companyLocation();
+				
+	}
+}
